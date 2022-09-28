e74e2bc3ff423845832d97585be5196a93592028
oangsa <sukruangkul.aongsa@gmail.com>
2022-09-28 19:18:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// const { WebhookClient } = require("discord.js")
 const PORT = 3000 || process.env.PORT
 const lineNotify = require('line-notify-nodejs')('pjLFmKaRFgJrgeO0WjGbqmloRIXpcj2VwdJQttDoCYr');
 const path = require("path");
